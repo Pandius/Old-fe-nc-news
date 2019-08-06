5edb4fd0c591ede2832d4366d821fee268f5c6f2
Pawel <pandius@poczta.fm>
2019-08-06 16:10:20 +0100

    added coding and football pictures with link to topics

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,18 @@ import React from 'react';
 import styles from './Home.module.css';
 import { Link } from '@reach/router';
 import coding from '../../pictures/coding.jpg'
+import cooking from '../../pictures/cooking.jpg'
+import football from '../../pictures/football.jpg'
 
 const Home = () => {
     return (
         <div className={styles.homepage}>
             <h1>TOPICS</h1>
             <Link to='/articles/topic/coding'><img className={styles.img} src={coding} alt={'coding'} /></Link>
+            <Link to='/articles/topic/cooking'><img classNAme={styles.img} src={cooking} alt={'cooking'} /></Link>
+            <Link to='/articles/topic/football'><img classNAme={styles.img} src={football} alt={'football'} /></Link>
+
+
 
 
         </div>
