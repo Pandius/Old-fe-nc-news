d58fef989d704eccb827b49460a435845821855e
Pawel <pandius@poczta.fm>
2019-08-06 11:50:27 +0100

    created api file with axios to fetch articles data from back-end

---
+++ src/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const BASE_URL = 'https://ncnewsbedos.herokuapp.com/api'
+
+export const getArticles = async (topic, sort_by, order, p) => {
+    const { data } = await axios.get(`${BASE_URL}/articles`, {
+        params: {
+            topic,
+            sort_by,
+            order, p
+        }
+    })
+    return data
+}
