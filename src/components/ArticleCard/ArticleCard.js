2b816399e55fe01d49022a154523a4861fbbe7c2
Pawel <pandius@poczta.fm>
2019-08-06 16:53:53 +0100

    sliced the body of the article so its only displaying 100 characters on article list for each

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -3,12 +3,13 @@ import { Link } from "@reach/router";
 import styles from './ArticleCard.module.css'
 
 const ArticleCard = ({ articles }) => {
+    const body = articles.body.slice(0, 100)
     return (
         <div>
             <li key={articles.article_id} className={styles.articlaCard}>
                 <Link to={`/articles/${articles.article_id}`}>
                     <h2>{articles.title}</h2> </Link>
-                <h5>{articles.body}</h5>
+                <h5>{body}...</h5>
                 <Link to={`/articles/topic/${articles.topic}`}>
                     <h5>Topic: {articles.topic}</h5></Link>
                 <h4>Author: {articles.author}</h4>
