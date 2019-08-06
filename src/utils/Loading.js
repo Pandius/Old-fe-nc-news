28457eae11f114565d4599c3e4a0474f47ad8ad4
Pawel <pandius@poczta.fm>
2019-08-06 12:27:16 +0100

    created utils folder with loading function

---
+++ src/utils/Loading.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import styles from '../styles/Loading.module.css'
+import LoadingImg from '../images/LoadingImg.gif'
+
+const Loading = ({ text }) => {
+    return (
+        <div>  
+            <div className={styles.loading}>
+                <h2>{text || 'loading...'}</h2>
+                <img src={LoadingImg} alt={'LoadingImg'}/>
+            </div>
+        </div>
+    )
+}
+
+export default Loading
