186465df4715128ebef60ea1e8604ed9bcb68734
Pawel <pandius@poczta.fm>
2019-08-08 15:47:28 +0100

    added description tags and changed pictures

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import styles from './Home.module.css';
 import { Link } from '@reach/router';
-import coding from '../../pictures/coding.jpg'
-import cooking from '../../pictures/cooking.jpg'
-import football from '../../pictures/football.jpg'
+import coding from '../../pictures/coding.gif'
+import cooking from '../../pictures/cooking.gif'
+import football from '../../pictures/football.gif'
 
 const Home = () => {
     return (
         <div className={styles.homepage}>
             <h1>TOPICS</h1>
+            <p> Coding:</p>
             <Link to='/articles/topic/coding'><img className={styles.img} src={coding} alt={'coding'} /></Link>
-            <Link to='/articles/topic/cooking'><img classNAme={styles.img} src={cooking} alt={'cooking'} /></Link>
-            <Link to='/articles/topic/football'><img classNAme={styles.img} src={football} alt={'football'} /></Link>
-
-
-
-
+            <p> Cooking:</p>
+            <Link to='/articles/topic/cooking'><img className={styles.img} src={cooking} alt={'cooking'} /></Link>
+            <p> Football:</p>
+            <Link to='/articles/topic/football'><img className={styles.img} src={football} alt={'football'} /></Link>
         </div>
     );
 };
