1b1c741d399b163d1649aff933cadd39ef8b5264
Pawel <pandius@poczta.fm>
2019-08-08 15:50:40 +0100

    added nav buttons styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,11 +6,11 @@ import styles from './Nav.module.css'
 const Nav = () => {
     return (
         <nav className={styles.Nav}>
-            <Link to='/'><button >Home</button> </Link>
-            <Link to='/articles'><button >All Articles</button></Link>
-            <Link to='/articles/topic/cooking'><button>Cooking</button></Link>
-            <Link to='/articles/topic/football'><button>Football</button></Link>
-            <Link to='/articles/topic/coding'><button>Coding</button></Link>
+            <Link to='/'><button className={styles.Button} >Home</button></Link>
+            <Link to='/articles'><button className={styles.Button} >All Articles</button></Link>
+            <Link to='/articles/topic/cooking'><button className={styles.Button}  >Cooking</button></Link>
+            <Link to='/articles/topic/football'><button className={styles.Button}>Football</button></Link>
+            <Link to='/articles/topic/coding'><button className={styles.Button}>Coding</button></Link>
         </nav>
     )
 }
