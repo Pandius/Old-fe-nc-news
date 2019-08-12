b7bd8ffb240070e0c720b50ab3c6e0b100e10701
Pawel <pandius@poczta.fm>
2019-08-12 10:39:33 +0100

    added delete and post comment

--- src/api.js
+++ src/api.js
@@ -14,6 +14,20 @@ export const getArticles = async (topic, sort_by, order, p) => {
     return data
 }
 
+export const postComment = async (article_id, newComment )=> {
+    const {data} = await axios.post(`${BASE_URL}/articles/${article_id}/comments`, newComment)
+    return data.comment
+}
+
+export const deleteCommentById = async comment_id => {
+    return axios.delete(`${BASE_URL}/comments/${comment_id}`)
+}
+
+export const getArticleComments = async article_id => {
+    const { data } = await axios.get(`${BASE_URL}/articles/${article_id}/comments`)
+    return data.comments
+}
+
 export const getArticleById = async article_id => {
     const { data } = await axios.get(`${BASE_URL}/articles/${article_id}`)
     return data.article
